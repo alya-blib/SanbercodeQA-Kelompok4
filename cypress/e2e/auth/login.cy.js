81ea457f521dc774fb41beeff8164f24b8bb49a6
alyasalsanabilamulyadi@gmail.com <alyasalsanabilamulyadi@gmail.com>
2023-06-04 22:54:57 +0700

    update login

--- cypress/e2e/auth/login.cy.js
+++ cypress/e2e/auth/login.cy.js
@@ -1,16 +1,37 @@
+import baseLogin from "../../support/PageObject/auth/baseLogin.cy.js"
+const loginData = require("../../fixtures/auth/dataLogin.json")
+
 describe('Login', () => {
+  const BaseLogin = new baseLogin()
     it('success login', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/')
-      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-      cy.get('.oxd-button').click()
-      cy.get('.oxd-brand-banner > img').should('be.visible')
+      cy.get(BaseLogin.usernameInput).type(loginData.usernameInput)
+      cy.get(BaseLogin.passwordInput).type(loginData.passwordInput)
+      cy.get(BaseLogin.loginButton).click()
+      cy.get(BaseLogin.beVisibleImg).should(loginData.beVisible)
     })
-    it('failed login', () => {
+    it('empty password login', () => {
         cy.visit('https://opensource-demo.orangehrmlive.com/')
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-        cy.get('.oxd-button').click()
-        cy.get('.oxd-input-group > .oxd-text').should('be.visible')
+        cy.get(BaseLogin.usernameInput).type(loginData.usernameInput)
+        cy.get(BaseLogin.passwordInput).clear()
+        cy.get(BaseLogin.loginButton).click()
+        cy.get(BaseLogin.bevisibleText).should(loginData.beVisible)
+        
+      })
+    it('empty username login', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
+        cy.get(BaseLogin.usernameInput).clear()
+        cy.get(BaseLogin.passwordInput).type(loginData.passwordInput)
+        cy.get(BaseLogin.loginButton).click()
+        cy.get(BaseLogin.bevisibleText).should(loginData.beVisible)
+        
+      })
+    it('invalid username login', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/')
+        cy.get(BaseLogin.usernameInput).type(loginData.usernameInvalid)
+        cy.get(BaseLogin.passwordInput).type(loginData.passwordInput)
+        cy.get(BaseLogin.loginButton).click()
+        cy.get(BaseLogin.alert).should('contain.text', loginData.invalidCredentian)
         
       })
 })
---
+++ cypress/support/PageObject/auth/baseLogin.cy.js
@@ -0,0 +1,11 @@
+class baseLogin {
+    usernameInput = ":nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    passwordInput = ":nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input"
+    loginButton = ".oxd-button"
+    beVisibleImg = ".oxd-brand-banner > img"
+    bevisibleText = ".oxd-input-group > .oxd-text"
+    alert = ".oxd-alert"
+  }
+  
+  export default baseLogin;
+  
