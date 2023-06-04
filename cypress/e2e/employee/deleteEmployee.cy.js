0806d52c76db682334025bd0c89d97c11a097cf6
alyasalsanabilamulyadi@gmail.com <alyasalsanabilamulyadi@gmail.com>
2023-06-04 09:21:55 +0700

    add locator employee

--- cypress/e2e/employee/deleteEmployee.cy.js
+++ cypress/e2e/employee/deleteEmployee.cy.js
@@ -1,33 +1,33 @@
-import baseAddEmployee from "../../support/PageObject/employee/baseAddEmployee.cy.js"
+import baseDeteleEmployee from "../../support/PageObject/employee/baseDeteleEmployee.cy.js"
 
 describe('delete employee', () => {
-    it.only('success delete employee', () => {
-      const BaseAddEmployee = new baseAddEmployee()
+    it('success delete employee', () => {
+      const BaseDeteleEmployee = new baseDeteleEmployee()
     // login
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin')
-      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-      cy.get('.oxd-button').click()
-      cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('contain.text', 'Dashboard')
+      cy.get(BaseDeteleEmployee.usernameInput).type('admin')
+      cy.get(BaseDeteleEmployee.passwordInput).type('admin123')
+      cy.get(BaseDeteleEmployee.loginButton).click()
+      cy.get(BaseDeteleEmployee.dashboard).should('contain.text', 'Dashboard')
 
-      cy.get(':nth-child(2) > .oxd-main-menu-item').click()
-      cy.get(':nth-child(1) > .oxd-table-row > :nth-child(9) > .oxd-table-cell-actions > :nth-child(1)').click({force: true})
-      cy.get('.oxd-button--label-danger').click()
-      cy.get('.oxd-toast').should('contain.text', 'Successfully Deleted')
+      cy.get(BaseDeteleEmployee.pimButton).click()
+      cy.get(BaseDeteleEmployee.deleteButton).click({force: true})
+      cy.get(BaseDeteleEmployee.yesDeleteButton).click()
+      cy.get(BaseDeteleEmployee.popupSuccess).should('contain.text', 'Successfully Deleted')
     })
 
     it('cancel delete employee', () => {
-      const BaseAddEmployee = new baseAddEmployee()
+      const BaseDeteleEmployee = new baseDeteleEmployee()
     // login
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin')
-      cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-      cy.get('.oxd-button').click()
-      cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('contain.text', 'Dashboard')
+      cy.get(BaseDeteleEmployee.usernameInput).type('admin')
+      cy.get(BaseDeteleEmployee.passwordInput).type('admin123')
+      cy.get(BaseDeteleEmployee.loginButton).click()
+      cy.get(BaseDeteleEmployee.dashboard).should('contain.text', 'Dashboard')
 
-      cy.get(':nth-child(2) > .oxd-main-menu-item').click()
-      cy.get(':nth-child(1) > .oxd-table-row > :nth-child(9) > .oxd-table-cell-actions > :nth-child(1)').click({force: true})
-      cy.get('.oxd-button--text').click()
+      cy.get(BaseDeteleEmployee.pimButton).click()
+      cy.get(BaseDeteleEmployee.deleteButton).click({force: true})
+      cy.get(BaseDeteleEmployee.buttonCancel).click()
     })
   })
   
